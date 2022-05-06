e25369db868e02263ea0cd5bd8453cf80efd43a6
Ignasi <ignasi@annamarco.com>
2022-05-06 14:43:52 +0200

    On branch master  Your branch is ahead of 'origin/master' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   dtw3_evaluation.py

---
+++ dtw3_evaluation.py
@@ -0,0 +1,2 @@
+
+# TODO: If some kind of "graphs, images, etc" hyperparameter dependat is needed, make use of a wandb logger (it admit run summaries with numbers, vectors, images, etc; open mind, wandb is not only for trainings [If it do not works, maybe it will be needed to end the script with a log step to commit...])
