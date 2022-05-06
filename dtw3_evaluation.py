2c06a4ff243feab0deab95d50af586b88b2e201c
Ignasi <ignasi@annamarco.com>
2022-05-06 14:51:42 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	renamed:    SLPDL_utils/dtw_metrics.py -> SLPDL_utils/dtw_validation.py
    	modified:   dtw3_evaluation.py

--- dtw3_evaluation.py
+++ dtw3_evaluation.py
@@ -1,2 +1,7 @@
 
 # TODO: If some kind of "graphs, images, etc" hyperparameter dependat is needed, make use of a wandb logger (it admit run summaries with numbers, vectors, images, etc; open mind, wandb is not only for trainings [If it do not works, maybe it will be needed to end the script with a log step to commit...])
+# dtw3_validation wer contains all the code to apply dtw and return the accuracy... (UGLY!!! I will have to change it).
+# dtw3_test test contains all the code to only generate predictions (OK!) but it needs 2 dataloaders instead of the only one it should.
+# Remember torch has a Concat dataset made
+# TODO: remake the list without random shuffling
+# TODO: make submission files and memory
